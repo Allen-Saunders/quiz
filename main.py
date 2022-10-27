977b0abd24c9f2edab5b14141fcc914db72e0748
kronic5179 <98897386+Allen-Saunders@users.noreply.github.com>
2022-10-27 17:22:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print(" who is the teacher least likely to hit the griddy?")
+print("a - mr kaune")
+print("b - mr braithewaite")
+print("c - mr crocker")
+print("b - mr white")
+a = True 
+b = False
+c = False
+d = False
+
